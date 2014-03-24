f5dd5739b779b729eec0d1ba6f7dd1425ac9f79a
Pierre Bourré <pierre.bourre@telecom-sudparis.eu>
2014-03-24 15:16:56 +0100

    Toujours pas possible de se servir de l'interface graphique, mais ça avance un minimum

--- software/java/src/affichage/Affichage.java
+++ software/java/src/affichage/Affichage.java
@@ -3,7 +3,7 @@ package affichage;
 public class Affichage {
 	Fenetre fen;
 	public static void main(String[] args) {      
-		Fenetre fen = new Fenetre();
+		//Fenetre fen = new Fenetre();
 	}
 
 }
