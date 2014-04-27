ed95fa505435042e098bc21962409d8baaede31f
PF <troll@yopmail.fr>
2014-04-28 00:16:30 +0200

    Le lanceur aussi

--- software/java/src/lanceur.java
+++ software/java/src/lanceur.java
@@ -1,7 +1,9 @@
-import hexapode.EnnemiException;
-import hexapode.GoToException;
+import hexapode.Direction;
+import hexapode.EnumPatte;
 import hexapode.Hexapode;
 import hexapode.Vec2;
+import hexapode.exceptions.EnnemiException;
+import hexapode.exceptions.GoToException;
 
 import java.util.Scanner;
 
@@ -14,10 +16,11 @@ import util.Sleep;
 /*
  * TODO list (méca/élec)
  * Cartes électroniques (alim/capteurs)
- * Capteur ultrason ou infrarouge + servo (on tourne le capteur dans la direction où on va)
+ * Capteur 2 ultrason ou infrarouge + servo (on tourne le capteur dans la direction où on va)
  * Jumper
  * Bouton d'arrêt d'urgence
  * Raspberry
+ * Lipo
  * Un ou deux boutons de manière à pouvoir le configurer sans pc?...
  * (un commutateur pour la couleur, un bouton poussoir pour le recalage)
  */
@@ -27,31 +30,35 @@ public class lanceur {
 	public static void main(String[] args) {
 		SerialManager serialmanager;
 		try {
-//		    serialmanager = new SerialManager();
-//			Serial serie = serialmanager.getSerial("serieAsservissement");
-			Hexapode hexa = new Hexapode(null, false);
+		    serialmanager = new SerialManager();
+			Serial serie = serialmanager.getSerial("serieAsservissement");
+			Hexapode hexa = new Hexapode(serie, false);
  			System.out.println("Attente");
 			Scanner scanner = new Scanner(System.in);
 			scanner.nextLine();
-
-            hexa.va_au_point(new Vec2(0, 100), true);
-            hexa.va_au_point(new Vec2(-10, 110), true);
-
+	
+	        Sleep.temps_defaut = 200;
+	        hexa.avancer(200);
+	        //	        hexa.setMarche(Hexapode.marche_basique);
+//	        hexa.setProfil(0);
+//            hexa.avancer(2000);
+//			hexa.poser_fresques();
+//            autre_marche_scriptee(hexa);
+//			test_validation(hexa);
+//			cercle(hexa);
+//			hexa.va_au_point(new Vec2(-300, 1500), true);
+//			marche_scriptee(hexa);
+//			hexa.avancer_tomber_feu(1000, EnumPatte.DROITE);
 //			hexa.recaler();
-
-            Hexapode hexaJaune = new Hexapode(null, false);
-            Hexapode hexaRouge = new Hexapode(null, true);
-
-            cercle(hexaJaune);
-            cercle(hexaRouge);
-
-            scanner.close();
-//			serie.close();
+			hexa.arret();
+			hexa.desasserv();
+			scanner.close();
+			serie.close();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-				
+		}				
 	}
+
 	public static void cercle(Hexapode hexa) throws GoToException, EnnemiException
 	{
         Sleep.temps_defaut = 300;
@@ -63,11 +70,79 @@ public class lanceur {
                 hexa.setDirectionRelatif(1);
         }	    
 	}
-	
+
+	// Tests de marche triphasée... mais ça ne marche pas.
+	// Peut-être à réessayer avec des patins anti-dérapants.
+	   public static void marche_triphase_1(Hexapode hexa) throws GoToException, EnnemiException
+	   {
+	        for(int i = 0; i < 10; i++)
+	        {
+	            hexa.goto_etat_triphase("ACBACB");
+	            hexa.goto_etat_triphase("BACBAC");
+                hexa.goto_etat_triphase("CBACBA");
+	        }
+	        hexa.arret();
+	    }
+
+       public static void marche_triphase_4(Hexapode hexa) throws GoToException, EnnemiException
+       {
+            for(int i = 0; i < 10; i++)
+            {
+                hexa.goto_etat_triphase("ABCABC");
+                hexa.goto_etat_triphase("BCABCA");
+                hexa.goto_etat_triphase("CABCAB");
+            }
+            hexa.arret();
+        }
+
+       public static void marche_triphase_2(Hexapode hexa) throws GoToException, EnnemiException
+       {
+            for(int i = 0; i < 10; i++)
+            {
+                hexa.goto_etat_triphase("ACBCBA");
+                hexa.goto_etat_triphase("BACACB");
+                hexa.goto_etat_triphase("CBABAC");
+            }
+            hexa.arret();
+        }
+
+       public static void marche_triphase_5(Hexapode hexa) throws GoToException, EnnemiException
+       {
+            for(int i = 0; i < 10; i++)
+            {
+                hexa.goto_etat_triphase("ABCCAB");
+                hexa.goto_etat_triphase("BCAABC");
+                hexa.goto_etat_triphase("CABBCA");
+            }
+            hexa.arret();
+        }
+
+       public static void marche_triphase_3(Hexapode hexa) throws GoToException, EnnemiException
+       {
+            for(int i = 0; i < 10; i++)
+            {
+                hexa.goto_etat_triphase("ACBBAC");
+                hexa.goto_etat_triphase("BACCBA");
+                hexa.goto_etat_triphase("CBAACB");
+            }
+            hexa.arret();
+        }
+
+       public static void marche_triphase_6(Hexapode hexa) throws GoToException, EnnemiException
+       {
+            for(int i = 0; i < 10; i++)
+            {
+                hexa.goto_etat_triphase("ABCBCA");
+                hexa.goto_etat_triphase("BCACAB");
+                hexa.goto_etat_triphase("CABABC");
+            }
+            hexa.arret();
+        }
+
 	public static void marche_scriptee(Hexapode hexa) throws GoToException, EnnemiException
 	{
-        Sleep.temps_defaut = 200;
-        for(int i = 0; i < 30; i++)
+        Sleep.temps_defaut = 600;
+        for(int i = 0; i < 15; i++)
         {
             hexa.goto_etat("101010");
             hexa.goto_etat("010101");
@@ -79,7 +154,7 @@ public class lanceur {
 	// par contre, celle-là est 3 fois plus lente
 	   public static void autre_marche_scriptee(Hexapode hexa) throws GoToException, EnnemiException
 	    {
-	        Sleep.temps_defaut = 200;
+	        Sleep.temps_defaut = 300;
 	        for(int i = 0; i < 30; i++)
 	        {
 	            if(i % 2 == 0)
